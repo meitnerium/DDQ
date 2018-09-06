10c071eb94d15ede8de2e2a834b1484a81b093b0
meitnerium <fdion2@chm.ulaval.ca>
2018-09-06 09:00:51 -0400

    adding exemple of speed test for mp2b

---
+++ exemple/speed_test/analyse_time.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 8 proc 46m52.954s
+# 4 proc 84m7.357s
+# 2 proc 
+# 1 proc 
+
+plt.plot([4,8],[46*60+53,84*60+7])
+plt.xlabel("Number of cores")
+plt.show()
